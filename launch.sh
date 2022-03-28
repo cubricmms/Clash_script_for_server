d7486828e5319ba153c34ff50f1ce9048b94e512
Charles Jing <cjing@mintel.com>
2022-03-28 16:54:45 +0800

    Refactor

--- launch.sh
+++ launch.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
 
 clashServiceUrl='http://localhost:9090'
-LEAST_LATENCY=1000
-SELECTOR=""
-
-back_func() {
-    declare -A MYGLOBAL
-    local vars
-    while :; do
-        ((MYGLOBAL["counter"]++))
-        IFS=\ / read -a vars <<< "$(</proc/uptime) $(</proc/loadavg)"
-        MYGLOBAL["uptime"]=$vars
-        MYGLOBAL["idle"]=${vars[1]}
-        MYGLOBAL["l01m"]=${vars[2]}
-        MYGLOBAL["l05m"]=${vars[3]}
-        MYGLOBAL["l15m"]=${vars[4]}
-        MYGLOBAL["active"]=${vars[5]}
-        MYGLOBAL["procs"]=${vars[6]}
-        MYGLOBAL["lpid"]=${vars[7]}
-        MYGLOBAL["rand"]=$RANDOM
-        MYGLOBAL["crt"]=$SECONDS
-        declare -p MYGLOBAL > /dev/shm/foo
-        sleep 1
-    done
-}
-
-dumpMyGlobal() {
-    . /dev/shm/foo
-    printf "%8s " ${!MYGLOBAL[@]}
-    echo
-    printf "%8s " ${MYGLOBAL[@]}
-    echo
-}
+echo 1000 >/dev/shm/clash-llatency
+echo "" >/dev/shm/clash-selector
+sleep .3;
 
 function waitForProxy {
     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $clashServiceUrl/proxies)" != "200" ]];
@@ -45,9 +17,11 @@ function waitForProxy {
 function testLatency {
     delayUrl="$clashServiceUrl/proxies/$1/delay"
     latency=`curl -s -G -X GET $delayUrl -d "timeout=300" -d "url=http://www.gstatic.com/generate_204" |jq ".delay" `
-    if [[ "$latency" != "null" ]] && test $latency -gt 0 && test $latency -lt $LEAST_LATENCY; then
-	    LEAST_LATENCY=$latency
-	    SELECTOR=$1
+    _least_latency=$(</dev/shm/clash-llatency)
+    if [[ "$latency" != "null" ]] && test $latency -gt 0 && test $latency -lt $_least_latency; then
+	    echo $latency >/dev/shm/clash-llatency
+	    echo $1 >/dev/shm/clash-selector
+        sleep .3;
 	    echo "$1 has faster connection, latency $latency"
     fi
 }
@@ -60,8 +34,13 @@ proxyList=`curl -s -X GET $clashServiceUrl/proxies | jq ".proxies.Proxy.all[]" |
 for proxy in $proxyList
 do
     echo Test proxy: $proxy...
-    testLatency $proxy 
+    testLatency $proxy &
 done
+wait
+
+SELECTOR=$(</dev/shm/clash-selector)
+LEAST_LATENCY=$(</dev/shm/clash-llatency)
+
 echo "Test finished! ${SELECTOR} has fastest connection with latency ${LEAST_LATENCY} ms!"
 
 SWITCH_URL="$clashServiceUrl/proxies/Proxy"
@@ -75,3 +54,6 @@ if test $STATUS_CODE -eq 204; then
 else
     echo "${STATUS_CODE}, switch failed";
 fi
+
+rm /dev/shm/clash-llatency
+rm /dev/shm/clash-selector
