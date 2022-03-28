21dadd3bd86cd084f172d1faa4fbaf36a436b464
Charles Jing <cjing@mintel.com>
2022-03-28 16:37:56 +0800

    Refactor

--- launch.sh
+++ launch.sh
@@ -1,79 +1,77 @@
 #!/bin/bash
-CONTAINER_NAME=clash-client
-docker stop $CONTAINER_NAME 2>/dev/null
 
-# update latest config file
-CLASH_CONFIG_LOCATION=$HOME/.config/clash
-mkdir -p $CLASH_CONFIG_LOCATION
-CONFIG_FILE=$CLASH_CONFIG_LOCATION/config.yaml
-if [ ! -f $CONFIG_FILE ]; then
-    wget -O $CONFIG_FILE  $CLASH_CONFIG_URL # source your env
-fi
-
-# download clash dashboard
-if [ ! -f ./gh-pages.zip ]; then
-    wget https://github.com/Dreamacro/clash-dashboard/archive/gh-pages.zip
-    unzip -o gh-pages.zip -d $CLASH_CONFIG_LOCATION
-fi
-
-# 7890 http
-# 7891 https
-# 9090 restful api controller
-
-docker run -d \
-    --rm \
-    --name $CONTAINER_NAME \
-    --env TZ=Asia/Shanghai \
-    -p 7890:7890 -p 7891:7891 -p 9090:9090 \
-    -v $CONFIG_FILE:/root/.config/clash/config.yaml \
-    -v $CLASH_CONFIG_LOCATION/clash-dashboard-gh-pages:/root/.config/clash/ui \
-    dreamacro/clash:v1.8.0
-
-# wait for services initiation
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9090/proxies)" != "200" ]];
-do
-    echo "Waiting proxy service.."
-    sleep 5;
-done
-echo "Proxy is ready!"
-
-# Summary latency for all proxies
-PROXIES=`curl -s -X GET http://0.0.0.0:9090/proxies | jq ".proxies.Proxy.all[]" | sed -r "s/\s+/%20/g"| sed -e "s/\"//g"`
-
-MIN_TIMEOUT=300
+clashServiceUrl='http://localhost:9090'
+LEAST_LATENCY=1000
 SELECTOR=""
 
-for PROXY in $PROXIES
-do
-    url="http://127.0.0.1:9090/proxies/${PROXY}/delay"
-    echo TESTING $url ...
-    latency=`curl -s -G -X GET $url -d "timeout=300" -d "url=http://www.gstatic.com/generate_204" |jq ".delay" `
+back_func() {
+    declare -A MYGLOBAL
+    local vars
+    while :; do
+        ((MYGLOBAL["counter"]++))
+        IFS=\ / read -a vars <<< "$(</proc/uptime) $(</proc/loadavg)"
+        MYGLOBAL["uptime"]=$vars
+        MYGLOBAL["idle"]=${vars[1]}
+        MYGLOBAL["l01m"]=${vars[2]}
+        MYGLOBAL["l05m"]=${vars[3]}
+        MYGLOBAL["l15m"]=${vars[4]}
+        MYGLOBAL["active"]=${vars[5]}
+        MYGLOBAL["procs"]=${vars[6]}
+        MYGLOBAL["lpid"]=${vars[7]}
+        MYGLOBAL["rand"]=$RANDOM
+        MYGLOBAL["crt"]=$SECONDS
+        declare -p MYGLOBAL > /dev/shm/foo
+        sleep 1
+    done
+}
 
-    if test "$latency" -gt 0 && test "$latency" -lt $MIN_TIMEOUT; then
-	    MIN_TIMEOUT=$latency
-	    SELECTOR=$PROXY
-	    echo "${PROXY} has faster connection"
+dumpMyGlobal() {
+    . /dev/shm/foo
+    printf "%8s " ${!MYGLOBAL[@]}
+    echo
+    printf "%8s " ${MYGLOBAL[@]}
+    echo
+}
+
+function waitForProxy {
+    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $clashServiceUrl/proxies)" != "200" ]];
+    do
+        echo "Waiting proxy service.."
+        sleep 22;
+    done
+    echo "Proxy is ready!"
+}
+
+function testLatency {
+    delayUrl="$clashServiceUrl/proxies/$1/delay"
+    latency=`curl -s -G -X GET $delayUrl -d "timeout=300" -d "url=http://www.gstatic.com/generate_204" |jq ".delay" `
+    if [[ "$latency" != "null" ]] && test $latency -gt 0 && test $latency -lt $LEAST_LATENCY; then
+	    LEAST_LATENCY=$latency
+	    SELECTOR=$1
+	    echo "$1 has faster connection, latency $latency"
     fi
+}
+
+waitForProxy
+
+# Summary latency for all proxies
+proxyList=`curl -s -X GET $clashServiceUrl/proxies | jq ".proxies.Proxy.all[]" | sed -r "s/\s+/%20/g"| sed -e "s/\"//g"`
+
+for proxy in $proxyList
+do
+    echo Test proxy: $proxy...
+    testLatency $proxy 
 done
+echo "Test finished! ${SELECTOR} has fastest connection with latency ${LEAST_LATENCY} ms!"
 
-echo "Test finished! ${SELECTOR} has fastest connection with latency ${MIN_TIMEOUT} ms!"
-
-curl -s -X GET http://127.0.0.1:9090/proxies | jq ".proxies.Proxy"
-
-SWITCH_URL="http://127.0.0.1:9090/proxies/Proxy"
-
+SWITCH_URL="$clashServiceUrl/proxies/Proxy"
 SELECTOR=`echo $SELECTOR | sed -e "s/%20/ /g"`
-
 POST=$( jq -n --arg parm "$SELECTOR" '{name: $parm}' )
-echo $POST
 
-STATUS_CODE=`curl -X PUT -w "%{http_code}" -d "${POST}" $SWITCH_URL`
+STATUS_CODE=`curl -s -X PUT -w "%{http_code}" -d "${POST}" $SWITCH_URL`
 
-echo $STATUS_CODE
 if test $STATUS_CODE -eq 204; then
     echo "Switch to ${SELECTOR} successfully!";
 else
     echo "${STATUS_CODE}, switch failed";
 fi
-
-curl -s -X GET http://127.0.0.1:9090/proxies | jq ".proxies.Proxy"
